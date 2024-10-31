48d8aa319f2a6638b5be21476c9f52595c5134c2
Zach Jaensch <zach.jaensch@safetyculture.io>
2024-10-31 13:46:48 +1100

    add back base path

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,26 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+
+  basePath: "/library",
+
+  // async redirects() {
+  //   // Return is expected to be a promise
+  //   return Promise.resolve([
+  //     {
+  //       source: "/",
+  //       destination: "/library",
+  //       basePath: false,
+  //       permanent: false,
+  //     },
+  //     {
+  //       source: "/:locale/library/:path*",
+  //       destination: "/library/:path*",
+  //       basePath: false,
+  //       permanent: false,
+  //     },
+  //   ]);
+  // },
 };
 
 export default nextConfig;
